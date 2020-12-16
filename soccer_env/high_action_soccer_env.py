f852299bbeb96a9221342aee9c612454df4e4faf
bcahlit <bcahlit@gmail.com>
2020-12-16 10:11:02 +0800

    add Intermediate reward

--- soccer_env/high_action_soccer_env.py
+++ soccer_env/high_action_soccer_env.py
@@ -157,12 +157,84 @@ class HighActionSoccerEnv(gym.Env, utils.EzPickle):
         action_type = ACTION_LOOKUP[action]
         self.env.act(action_type)
 
+    #  def _get_reward(self):
+        #  """ Reward is given for scoring a goal. """
+        #  if self.status == hfo_py.GOAL:
+            #  return 1
+        #  else:
+            #  return 0
     def _get_reward(self):
-        """ Reward is given for scoring a goal. """
+        """
+        Agent is rewarded for minimizing the distance between itself and
+        the ball, minimizing the distance between the ball and the goal,
+        and scoring a goal.
+        """
+        current_state = self.env.getState()
+        #print("State =",current_state)
+        #print("len State =",len(current_state))
+        ball_proximity = current_state[53]
+        goal_proximity = current_state[15]
+        ball_dist = 1.0 - ball_proximity
+        goal_dist = 1.0 - goal_proximity
+        kickable = current_state[12]
+        ball_ang_sin_rad = current_state[51]
+        ball_ang_cos_rad = current_state[52]
+        ball_ang_rad = math.acos(ball_ang_cos_rad)
+        if ball_ang_sin_rad < 0:
+            ball_ang_rad *= -1.
+        goal_ang_sin_rad = current_state[13]
+        goal_ang_cos_rad = current_state[14]
+        goal_ang_rad = math.acos(goal_ang_cos_rad)
+        if goal_ang_sin_rad < 0:
+            goal_ang_rad *= -1.
+        alpha = max(ball_ang_rad, goal_ang_rad) - min(ball_ang_rad,
+                                                      goal_ang_rad)
+        ball_dist_goal = math.sqrt(ball_dist * ball_dist +
+                                   goal_dist * goal_dist - 2. * ball_dist *
+                                   goal_dist * math.cos(alpha))
+        # Compute the difference in ball proximity from the last step
+        if not self.first_step:
+            ball_prox_delta = ball_proximity - self.old_ball_prox
+            kickable_delta = kickable - self.old_kickable
+            ball_dist_goal_delta = ball_dist_goal - self.old_ball_dist_goal
+        self.old_ball_prox = ball_proximity
+        self.old_kickable = kickable
+        self.old_ball_dist_goal = ball_dist_goal
+        reward = 0
+        if not self.first_step:
+            mtb = self.__move_to_ball_reward(kickable_delta, ball_prox_delta)
+            ktg = 3. * self.__kick_to_goal_reward(ball_dist_goal_delta)
+            eot = self.__EOT_reward()
+            reward = mtb + ktg + eot
+            #print("mtb: %.06f ktg: %.06f eot: %.06f"%(mtb,ktg,eot))
+
+        self.first_step = False
+        #print("r =",reward)
+        return reward
+
+    def __move_to_ball_reward(self, kickable_delta, ball_prox_delta):
+        reward = 0.
+        if self.env.playerOnBall().unum < 0 or self.env.playerOnBall(
+        ).unum == self.unum:
+            reward += ball_prox_delta
+        if kickable_delta >= 1 and not self.got_kickable_reward:
+            reward += 1.
+            self.got_kickable_reward = True
+        return reward
+
+    def __kick_to_goal_reward(self, ball_dist_goal_delta):
+        if (self.env.playerOnBall().unum == self.unum):
+            return -ball_dist_goal_delta
+        elif self.got_kickable_reward == True:
+            return 0.2 * -ball_dist_goal_delta
+        return 0.
+
+    def __EOT_reward(self):
         if self.status == hfo_py.GOAL:
-            return 1
-        else:
-            return 0
+            return 5.
+        elif self.status == hfo_py.CAPTURED_BY_DEFENSE:
+            return -1.
+        return 0.
 
     def reset(self):
         """ Repeats NO-OP action until a new episode begins. """
